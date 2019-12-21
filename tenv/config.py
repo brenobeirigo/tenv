ddf9398812aa7e8e460f95025ff6978372dbd87a
brenobeirigo <brenobeirigo@gmail.com>
2019-12-21 19:31:34 +0100

    Shortened names

--- tenv/config.py
+++ tenv/config.py
@@ -15,15 +15,15 @@ def get_excerpt_name(start, stop, label="excerpt"):
 
 
 # TODO create dictionary of paths indexed by the region type
-REGION_CONCENTRIC = "CONCENTRIC"
-REGION_REGULAR = "MINCOVERSET"
+REGION_CONCENTRIC = "CON"
+REGION_REGULAR = "REG"
 
 # How regions are sliced?
 # region_slice = REGION_CONCENTRIC
 region_slice = REGION_REGULAR
 
 label_exp = "GIANT"
-
+short_path = True
 # root = os.getcwd().replace("\\", "/")
 root = "C:/Users/LocalAdmin/OneDrive/leap_forward/street_network_server/tenv"
 # root = "C:/Users/breno/Documents/phd/tenv"
@@ -42,6 +42,10 @@ region = tripdata["region"]
 
 # Create and store graph name
 graph_name = tripdata["region"].lower().replace(" ", "-").replace(",", "")
+
+if short_path:
+    graph_name = graph_name[:4]
+    label_exp = label_exp[:1]
 
 # Where dataset is saved
 root_path = root + "/data/out/{}{}".format(label_exp, graph_name)
@@ -98,11 +102,11 @@ if "url_tripdata" in tripdata:
 
 root_dist = root_path + "/distance"
 # Distance matrix
-path_dist_matrix = "{}/distance_matrix_m_{}.csv".format(root_dist, graph_name)
+path_dist_matrix = "{}/matrix_m_{}.csv".format(root_dist, graph_name)
 # Distance dictionary (meters)
-path_dist_dic = "{}/distance_dic_m_{}.npy".format(root_dist, graph_name)
+path_dist_dic = "{}/dic_m_{}.npy".format(root_dist, graph_name)
 
-path_dist_matrix_npy = "{}/distance_matrix_m_{}.npy".format(
+path_dist_matrix_npy = "{}/matrix_m_{}.npy".format(
     root_dist, graph_name
 )
 
@@ -118,7 +122,7 @@ total_range = 1200
 speed_km_h = 20
 round_trip = False
 # Maximum number of node neighbors queried by application
-max_neighbors = 4
+max_neighbors = 8
 step_list = [0, 60, 300, 600]
 step_list_concentric = [60, 300, 600]
 
@@ -127,7 +131,7 @@ step_list_concentric = [60, 300, 600]
 max_travel_time_edge = 30
 
 # Max. time to execute ilp (min)
-ilp_time_limit = 60
+ilp_time_limit = 120
 
 round_trip_label = "_roundtrip" if round_trip else ""
 
@@ -139,9 +143,9 @@ root_reachability = root_map + "/reachability_{}{}_{}_{}{}".format(
     ("_kmh{}".format(speed_km_h) if speed_km_h else ""),
 )
 
-root_img_regions = root_reachability + "/img_region_centers"
+root_img_regions = root_reachability + "/img_rcs"
 
-root_img_neighbors = root_reachability + "/img_region_center_neighbors"
+root_img_neighbors = root_reachability + "/img_rcs_neighbors"
 
 # Reachability dictionary {o:{max_dist:[d1, d2, d3]}
 path_reachability_dic = "{}/reachability_{}.npy".format(
@@ -154,19 +158,19 @@ path_reachability_r_dic = "{}/reachability_r_{}.npy".format(
 )
 
 # Region centers dictionary {max_dist:[c1, c2, c3, c4, c5]}
-path_region_centers = "{}/region_centers_{}.npy".format(
+path_region_centers = "{}/rcs_{}.npy".format(
     root_reachability, graph_name
 )
 
-path_region_center_ids = "{}/region_center_ids_{}.npy".format(
+path_region_center_ids = "{}/rcs_ids_{}.npy".format(
     root_reachability, graph_name
 )
 
-path_sorted_neighbors = "{}/sorted_neighbors_region_centers_{}.npy".format(
+path_sorted_neighbors = "{}/sorted_neighbors_rcs_{}.npy".format(
     root_reachability, graph_name
 )
 
-path_sorted_neighbors_lean = "{}/sorted_neighbors_region_centers_{}_{}.npy".format(
+path_sorted_neighbors_lean = "{}/sorted_neighbors_rcs_{}_{}.npy".format(
     root_reachability, graph_name, "_".join([str(t) for t in step_list])
 )
 
